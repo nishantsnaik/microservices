0e144ea77a40373ee663eeeb2edb0f36ef93a950
Nishant Naik <>
2018-05-24 12:51:24 -0400

    code commit

---
+++ currency-conversion-service/src/main/java/com/nishant/microservices/currencyconversionservice/CurrencyConversionBean.java
@@ -0,0 +1,77 @@
+package com.nishant.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+
+public class CurrencyConversionBean {
+	
+	private long id;
+	private String from;
+	private String to;
+	private BigDecimal conversionMultiple;
+	private BigDecimal quantity;
+	private BigDecimal totalCalculatedAmout;
+	private int port;
+	public CurrencyConversionBean(long id, String from, String to, BigDecimal conversionMultiple, BigDecimal quantity,
+			BigDecimal totalCalculatedAmout, int port) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+		this.quantity = quantity;
+		this.totalCalculatedAmout = totalCalculatedAmout;
+		this.port = port;
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getFrom() {
+		return from;
+	}
+	public void setFrom(String from) {
+		this.from = from;
+	}
+	public String getTo() {
+		return to;
+	}
+	public void setTo(String to) {
+		this.to = to;
+	}
+	public BigDecimal getConversionMultiple() {
+		return conversionMultiple;
+	}
+	public void setConversionMultiple(BigDecimal conversionMultiple) {
+		this.conversionMultiple = conversionMultiple;
+	}
+	public BigDecimal getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(BigDecimal quantity) {
+		this.quantity = quantity;
+	}
+	public BigDecimal getTotalCalculatedAmout() {
+		return totalCalculatedAmout;
+	}
+	public void setTotalCalculatedAmout(BigDecimal totalCalculatedAmout) {
+		this.totalCalculatedAmout = totalCalculatedAmout;
+	}
+	public int getPort() {
+		return port;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public CurrencyConversionBean() {
+		
+	}
+	
+	
+	
+	
+	
+	
+
+}
